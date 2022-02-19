f48f683ff93d8c64575cbb2386dbf3b2765f7980
aakanksha1two <55326446+aakanksha1two@users.noreply.github.com>
2022-02-20 00:08:15 +0530

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,25 +1,35 @@
 #include<iostream>
-using namesoace std;
-int factorial(int num){
+using namespace std;
+int 
+int factorial(int num);
+int kcombination(int n,int k );
+
+int main ()
+{
+
+int n,k
+cout<<"Please enter n,k (k<=n)"<<endl;
+cin>>n>>k;
+cout<<n<<"choose"<<k<<"="<<k_combination(n,k)<<endl;
+return 0;
+
+}
+int k_combination(int n,int k)
+{
+  int nFact,kFact,k_nFact,i,k_comb;
+  nFact=factorial(n);
+kFact=factorial(k);
+k_nFact=factorial(n-k);
+  k_comb=int(nFact/(kFact*k_nFact));
+  return k_comb;
+}
+
+k_comb=int(nFact/(kFact*k_nFact));
+int factorial(int num)
+{
 int f=1;
 for (int i=1;i<=num;i++)
 f=f*i;
 return f;
-}
-int main ()
-{
-
-int n,k,nFact,kFact,k_nFact,i,k_comb
-cout<<"Please enter n,k (k<=n)"<<endl;
-cin>>n>>k;
-
-nFact=factorial(n);
-kFact=factorial(k);
-k_nFact=factorial(n-k);
-
-k_comb=int(nFact/(kFact*k_nFact));
-cout<<n<<"choose"<<k<<"="<<k_comb<<endl;
-return 0;
-
 }
 
